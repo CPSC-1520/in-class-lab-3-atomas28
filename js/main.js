35f47211f3b78d11a3d338adb5d3e7ef6b997f78
Austin Tomas <115746501+atomas28@users.noreply.github.com>
2024-10-01 19:53:09 -0600

    Step 2 done :)

--- js/main.js
+++ js/main.js
@@ -2,12 +2,31 @@
 
 
 //STEP 1
-let rescources = document.querySelector(".javascript-resources");
+let resources = document.querySelector(".javascript-resources");
 let showButton = document.querySelector("#show-resources");
 
 showButton.addEventListener('click', function(event){
-    rescources.classList.remove("d-none");
+    resources.classList.remove("d-none");
 });
+
+
+//STEP 2
+
+// 1. Add an event listener on element selected in the previous step that will listen to the mouseover
+// event. Ensure that you’re passing in the event object to the event handler function.
+
+// 2. Use console.log to print out the “event.target” to see the link list item elements are the
+// event.target returned
+
+
+// 3. On the event.target use the method “classList.add” to add the class named “fw-bold”.
+
+// a. When you only hover over the the first link item you should see something like this.
+
+resources.addEventListener('mouseover',function(event){
+    console.log(event.target);
+    event.target.classList.add("fw-bold");
+})
 
 
 

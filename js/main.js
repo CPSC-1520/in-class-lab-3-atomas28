ba031df7e43310e5dcf1ad77e2819e33c13243f2
Austin Tomas <115746501+atomas28@users.noreply.github.com>
2024-10-01 19:43:53 -0600

    Step 1 Done :)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // Enter your code below.
+
+
+//STEP 1
+let rescources = document.querySelector(".javascript-resources");
+let showButton = document.querySelector("#show-resources");
+
+showButton.addEventListener('click', function(event){
+    rescources.classList.remove("d-none");
+});
+
+
+
+
